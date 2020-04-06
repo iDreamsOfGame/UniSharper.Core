2331b25623a549c76a92ad5ccdc197bd56891c12
Jerry Lee <cosmos53076@163.com>
2020-04-06 21:05:30 +0800

    Initialize unity project.

---
+++ Assets/UniSharper.Core/Scripts/Runtime/Rendering/PrefabLightmapExcludedRenderer.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Jerry Lee. All rights reserved. Licensed under the MIT License. See LICENSE in the
+// project root for license information.
+
+using UnityEngine;
+
+namespace UniSharper.Rendering
+{
+    /// <summary>
+    /// The <see cref="PrefabLightmapExcludedRenderer"/> to define the excluded GameObject that no
+    /// need to store lightmap data.
+    /// </summary>
+    /// <seealso cref="MonoBehaviour"/>
+    [RequireComponent(typeof(MeshRenderer))]
+    public sealed class PrefabLightmapExcludedRenderer : MonoBehaviour
+    {
+    }
+}
