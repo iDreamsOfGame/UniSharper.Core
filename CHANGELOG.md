c6b8aeb6e7b9b49d83389ad7d3789049d45bddf8
Jerry Lee <cosmos53076@163.com>
2023-11-24 22:13:44 +0800

    Adds class SearchPattern.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 
 
+## [1.16.0] - 2023-11-24
+
+### Added
+
+- Adds class **SearchPattern** that include common files search patterns.
+
+
+
 ## [1.15.0] - 2023-11-24
 
 ### Added
---
+++ Runtime/SearchPattern.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Jerry Lee. All rights reserved. Licensed under the MIT License. See LICENSE in the
+// project root for license information.
+
+namespace UniSharper
+{
+    /// <summary>
+    /// The collection of search pattern constant.
+    /// </summary>
+    public sealed class SearchPattern
+    {
+        /// <summary>
+        /// The search pattern of binary files.
+        /// </summary>
+        public const string BinaryFiles = "*.bytes";
+
+        /// <summary>
+        /// The search pattern of prefab files.
+        /// </summary>
+        public const string PrefabFiles = "*.prefab";
+
+        /// <summary>
+        /// The search pattern of PNG image files.
+        /// </summary>
+        public const string PngImageFiles = "*.png";
+
+        /// <summary>
+        /// The search pattern of old format excel files.
+        /// </summary>
+        public const string ExcelFiles = "*.xls";
+
+        /// <summary>
+        /// The search pattern of new format excel files.
+        /// </summary>
+        public const string ExcelXFiles = "*.xlsx";
+    }
+}
