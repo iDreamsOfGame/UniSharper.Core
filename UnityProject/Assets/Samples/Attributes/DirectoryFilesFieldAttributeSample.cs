9c810e7cb57b2df1c9f00f765777d043a355e6bc
Jerry Lee <cosmos53076@163.com>
2023-12-20 16:52:27 +0800

    Updates submodule.

--- UnityProject/Assets/Samples/Attributes/DirectoryFilesFieldAttributeSample.cs
+++ UnityProject/Assets/Samples/Attributes/DirectoryFilesFieldAttributeSample.cs
@@ -7,16 +7,16 @@ namespace UniSharper.Samples
 {
     internal class DirectoryFilesFieldAttributeSample : MonoBehaviour
     {
-        [SerializeField, DirectoryFilesField(PlayerEnvironment.AssetsFolderName, SearchPattern.CSharpScriptFiles, SearchOption.AllDirectories, true)]
+        [SerializeField, DirectoryFilesField(PlayerEnvironment.AssetsFolderName, SearchPatterns.CSharpScriptFiles, SearchOption.AllDirectories, true)]
         private string fileName;
 
-        [SerializeField, DirectoryFilesField(PlayerEnvironment.AssetsFolderName, SearchPattern.CSharpScriptFiles, SearchOption.AllDirectories)]
+        [SerializeField, DirectoryFilesField(PlayerEnvironment.AssetsFolderName, SearchPatterns.CSharpScriptFiles, SearchOption.AllDirectories)]
         private string fileNameWithoutExtension;
 
-        [SerializeField, DirectoryFilesField(new[] { PlayerEnvironment.AssetsFolderName, "Packages/com.unity.ugui" }, SearchPattern.CSharpScriptFiles, SearchOption.AllDirectories, true)]
+        [SerializeField, DirectoryFilesField(new[] { PlayerEnvironment.AssetsFolderName, "Packages/com.unity.ugui" }, SearchPatterns.CSharpScriptFiles, SearchOption.AllDirectories, true)]
         private string fileNameInDirectories;
         
-        [SerializeField, DirectoryFilesField(new[] { PlayerEnvironment.AssetsFolderName, "Packages/com.unity.ugui" }, SearchPattern.CSharpScriptFiles, SearchOption.AllDirectories, true, true)]
+        [SerializeField, DirectoryFilesField(new[] { PlayerEnvironment.AssetsFolderName, "Packages/com.unity.ugui" }, SearchPatterns.CSharpScriptFiles, SearchOption.AllDirectories, true, true)]
         private string fileNameInDirectoriesWithDescendingOrder;
     }
 }
