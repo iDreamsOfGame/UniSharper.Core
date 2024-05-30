cc6a2c5c28a3e2ead93db97b8569344b76f9be94
Jerry Lee <cosmos53076@163.com>
2024-05-30 18:57:31 +0800

    Updates submodule.

--- UnityProject/Assets/Samples/Effect/ParticleEffectControllerSample.cs
+++ UnityProject/Assets/Samples/Effect/ParticleEffectControllerSample.cs
@@ -8,27 +8,29 @@ namespace UniSharper.Samples
         [SerializeField]
         private ParticleEffectController effectController;
 
+        private void OnEffectPlayStarted(IParticleEffectController particleEffectController)
+        {
+            Debug.Log("effect start!");
+        }
+        
         private void OnEffectPaused(IParticleEffectController particleEffectController)
         {
             Debug.Log("effect paused!");
         }
 
-        private void OnEffectPlayCompleted(IParticleEffectController particleEffectController)
-        {
-            Debug.Log("effect stopped!");
-
-            if (!effectController.IsLoop)
-                Destroy(effectController.gameObject);
-        }
-
-        private void OnEffectPlayStarted(IParticleEffectController particleEffectController)
-        {
-            Debug.Log("effect start!");
-        }
-
         private void OnEffectResumed(IParticleEffectController particleEffectController)
         {
             Debug.Log("effect resumed!");
+        }
+        
+        private void OnEffectStopped(IParticleEffectController particleEffectController)
+        {
+            Debug.Log($"effect stopped, duration: {particleEffectController.Duration}.");
+        }
+
+        private void OnEffectLoopPointReached(IParticleEffectController particleEffectController)
+        {
+            Debug.Log($"effect loop reached, duration: {particleEffectController.Duration}.");
         }
 
         private void Pause()
@@ -47,9 +49,12 @@ namespace UniSharper.Samples
             effectController.Started.AddListener(OnEffectPlayStarted);
             effectController.Paused.AddListener(OnEffectPaused);
             effectController.Resumed.AddListener(OnEffectResumed);
-            effectController.LoopPointReached.AddListener(OnEffectPlayCompleted);
+            effectController.LoopPointReached.AddListener(OnEffectLoopPointReached);
+            effectController.Stopped.AddListener(OnEffectStopped);
             effectController.Play();
-            Invoke(nameof(Pause), 0.5f);
+            
+            if (!effectController.isActiveAndEnabled)
+                Invoke(nameof(Pause), 0.5f);
         }
     }
 }
