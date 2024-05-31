d5681158c9cac784e2b65c9cba8ebaea9da96452
Jerry Lee <cosmos53076@163.com>
2024-05-31 14:59:44 +0800

    Updates submodule and sample.

--- UnityProject/Assets/Samples/Effect/ParticleEffectControllerSample.cs
+++ UnityProject/Assets/Samples/Effect/ParticleEffectControllerSample.cs
@@ -6,55 +6,64 @@ namespace UniSharper.Samples
     internal class ParticleEffectControllerSample : MonoBehaviour
     {
         [SerializeField]
-        private ParticleEffectController effectController;
+        private ParticleEffectController nonLoopEffectController;
+
+        [SerializeField]
+        private ParticleEffectController loopEffectController;
 
         private void OnEffectPlayStarted(IParticleEffectController particleEffectController)
         {
-            Debug.Log("effect start!");
+            Debug.Log($"effect ({particleEffectController.CachedTransform.name}) start!");
         }
         
         private void OnEffectPaused(IParticleEffectController particleEffectController)
         {
-            Debug.Log("effect paused!");
+            Debug.Log($"effect({particleEffectController.CachedTransform.name}) paused!");
         }
 
         private void OnEffectResumed(IParticleEffectController particleEffectController)
         {
-            Debug.Log("effect resumed!");
+            Debug.Log($"effect({particleEffectController.CachedTransform.name}) resumed!");
         }
         
         private void OnEffectStopped(IParticleEffectController particleEffectController)
         {
-            Debug.Log($"effect stopped, duration: {particleEffectController.Duration}.");
+            Debug.Log($"effect({particleEffectController.CachedTransform.name}) stopped, playback time: {particleEffectController.PlaybackTime}, duration: {particleEffectController.Duration}.");
         }
 
         private void OnEffectLoopPointReached(IParticleEffectController particleEffectController)
         {
-            Debug.Log($"effect loop reached, duration: {particleEffectController.Duration}.");
+            Debug.Log($"effect({particleEffectController.CachedTransform.name}) loop reached, duration: {particleEffectController.Duration}.");
         }
 
         private void Pause()
         {
-            effectController.Pause();
+            nonLoopEffectController.Pause();
+            loopEffectController.Pause();
             Invoke(nameof(Resume), 1f);
         }
 
         private void Resume()
         {
-            effectController.Resume();
+            nonLoopEffectController.Resume();
+            loopEffectController.Resume();
         }
 
         private void Start()
         {
-            effectController.Started.AddListener(OnEffectPlayStarted);
-            effectController.Paused.AddListener(OnEffectPaused);
-            effectController.Resumed.AddListener(OnEffectResumed);
-            effectController.LoopPointReached.AddListener(OnEffectLoopPointReached);
-            effectController.Stopped.AddListener(OnEffectStopped);
-            effectController.Play();
+            nonLoopEffectController.Started.AddListener(OnEffectPlayStarted);
+            nonLoopEffectController.Paused.AddListener(OnEffectPaused);
+            nonLoopEffectController.Resumed.AddListener(OnEffectResumed);
+            nonLoopEffectController.Stopped.AddListener(OnEffectStopped);
+            nonLoopEffectController.Play();
             
-            if (!effectController.isActiveAndEnabled)
-                Invoke(nameof(Pause), 0.5f);
+            loopEffectController.Started.AddListener(OnEffectPlayStarted);
+            loopEffectController.Paused.AddListener(OnEffectPaused);
+            loopEffectController.Resumed.AddListener(OnEffectResumed);
+            loopEffectController.LoopPointReached.AddListener(OnEffectLoopPointReached);
+            loopEffectController.Play();
+            
+            Invoke(nameof(Pause), 0.5f);
         }
     }
 }
