3eb367826bdedf2580b8369464218395ad6aa819
Jerry Lee <cosmos53076@163.com>
2024-05-31 10:23:23 +0800

    Changes method ParticleEffectController.Initialize to virtual.

--- Runtime/Effect/ParticleEffectController.cs
+++ Runtime/Effect/ParticleEffectController.cs
@@ -108,7 +108,7 @@ namespace UniSharper.Effect
 
         public ParticleEffectEvent Stopped => stoppedEvent ??= new ParticleEffectEvent();
 
-        public void Initialize()
+        public virtual void Initialize()
         {
             if (HasInitialized)
                 return;
