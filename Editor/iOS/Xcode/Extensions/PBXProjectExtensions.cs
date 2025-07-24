af835e98ff715eb05cd373db9580f8e2d5d0d184
Jerry Lee <cosmos53076@163.com>
2025-07-24 20:08:23 +0800

    Fixed bugs.

--- Editor/iOS/Xcode/Extensions/PBXProjectExtensions.cs
+++ Editor/iOS/Xcode/Extensions/PBXProjectExtensions.cs
@@ -20,17 +20,17 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
         /// </summary>
         /// <param name="pbxProject">The Xcode project. </param>
         /// <param name="realPath">The physical path to the folder on the filesystem. </param>
-        /// <param name="projectPath">The project path to the folder. </param>
+        /// <param name="rootProjectPath">The root project path of the group to be created. </param>
         /// <param name="filesExclusive">The exclusive files list. </param>
         /// <param name="dirsExclusive">The exclusive directories list. </param>
         public static void CreateGroup(this PBXProject pbxProject,
             string realPath,
-            string projectPath,
+            string rootProjectPath,
             string[] filesExclusive = null,
             string[] dirsExclusive = null)
         {
             var targetGuid = pbxProject.GetUnityMainTargetGuid();
-            pbxProject.CreateGroup(targetGuid, realPath, projectPath, filesExclusive, dirsExclusive);
+            pbxProject.CreateGroup(targetGuid, realPath, rootProjectPath, filesExclusive, dirsExclusive);
         }
         
         /// <summary>
@@ -39,13 +39,13 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
         /// <param name="pbxProject">The Xcode project. </param>
         /// <param name="targetGuid">The GUID of the target as returned by <see cref="PBXProject.TargetGuidByName"/>. </param>
         /// <param name="realPath">The physical path to the folder on the filesystem. </param>
-        /// <param name="projectPath">The project path to the folder. </param>
+        /// <param name="rootProjectPath">The root project path of the group to be created. </param>
         /// <param name="filesExclusive">The exclusive files list. </param>
         /// <param name="dirsExclusive">The exclusive directories list. </param>
         public static void CreateGroup(this PBXProject pbxProject,
             string targetGuid,
             string realPath,
-            string projectPath,
+            string rootProjectPath,
             string[] filesExclusive = null,
             string[] dirsExclusive = null)
         {
@@ -60,8 +60,8 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
                 if (filesExclusive != null && filesExclusive.Contains(fileName))
                     continue;
 
-                var projectFilePath = Path.Combine(projectPath, fileName);
-                var fileGuid = pbxProject.AddFile(projectFilePath, projectFilePath);
+                var fileProjectPath = Path.Combine(rootProjectPath, fileName);
+                var fileGuid = pbxProject.AddFile(fileProjectPath, fileProjectPath);
                 pbxProject.AddFileToBuild(targetGuid, fileGuid);
             }
 
@@ -73,7 +73,7 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
                 if (dirsExclusive != null && dirsExclusive.Contains(dirName))
                     continue;
 
-                var subDirProjectPath = Path.Combine(projectPath, dirName);
+                var subDirProjectPath = Path.Combine(rootProjectPath, dirName);
                 if (subDirRealPath.EndsWith(FileExtensions.XcodeAssetsFile))
                 {
                     var fileGuid = pbxProject.AddFile(subDirProjectPath, subDirProjectPath);
@@ -96,19 +96,19 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
         /// </summary>
         /// <param name="pbxProject">The Xcode project. </param>
         /// <param name="realPath">The physical path to the folder on the filesystem of the group. </param>
-        public static void RemoveGroup(this PBXProject pbxProject, string realPath)
+        public static void RemoveGroupByRealPath(this PBXProject pbxProject, string realPath)
         {
             var targetGuid = pbxProject.GetUnityMainTargetGuid();
-            pbxProject.RemoveGroup(targetGuid, realPath);
+            pbxProject.RemoveGroupByRealPath(targetGuid, realPath);
         }
         
         /// <summary>
-        /// Removes a group reference from the specified target in Xcode project.
+        /// Removes a group reference from the specified target in Xcode project by real path.
         /// </summary>
         /// <param name="pbxProject">The Xcode project. </param>
         /// <param name="targetGuid">The GUID of the target as returned by <see cref="PBXProject.TargetGuidByName"/>. </param>
         /// <param name="realPath">The physical path to the folder on the filesystem of the group. </param>
-        public static void RemoveGroup(this PBXProject pbxProject, string targetGuid, string realPath)
+        public static void RemoveGroupByRealPath(this PBXProject pbxProject, string targetGuid, string realPath)
         {
             // 移除文件引用
             var files = Directory.GetFiles(realPath);
@@ -146,7 +146,77 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
                 }
                 else
                 {
-                    pbxProject.RemoveGroup(targetGuid, subDirRealPath);
+                    pbxProject.RemoveGroupByRealPath(targetGuid, subDirRealPath);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Removes a group reference from the UnityMain target in Xcode project by project path.
+        /// </summary>
+        /// <param name="pbxProject">The Xcode project. </param>
+        /// <param name="realPath">The physical path to the folder on the filesystem of the group. </param>
+        /// <param name="rootProjectPath">The root project path of group to be removed. </param>
+        public static void RemoveGroupByProjectPath(this PBXProject pbxProject, string realPath, string rootProjectPath)
+        { 
+            var targetGuid = pbxProject.GetUnityMainTargetGuid();
+            pbxProject.RemoveGroupByProjectPath(targetGuid, realPath, rootProjectPath);
+        }
+
+        /// <summary>
+        /// Removes a group reference from the specified target in Xcode project by project path.
+        /// </summary>
+        /// <param name="pbxProject">The Xcode project. </param>
+        /// <param name="targetGuid">The GUID of the target as returned by <see cref="PBXProject.TargetGuidByName"/>. </param>
+        /// <param name="realPath">The physical path to the folder on the filesystem of the group. </param>
+        /// <param name="rootProjectPath">The root project path of group to be removed. </param>
+        public static void RemoveGroupByProjectPath(this PBXProject pbxProject,
+            string targetGuid,
+            string realPath,
+            string rootProjectPath)
+        {
+            // 移除文件引用
+            var files = Directory.GetFiles(realPath);
+            foreach (var file in files)
+            {
+                if (file.EndsWith(FileExtensions.MacOSXDesktopServicesStoreFile))
+                    continue;
+                
+                var fileName = Path.GetFileName(file);
+                var fileProjectPath = $"{rootProjectPath}/{fileName}";
+                var fileGuid = pbxProject.FindFileGuidByProjectPath(fileProjectPath);
+                if (string.IsNullOrEmpty(fileGuid))
+                    continue;
+                
+                pbxProject.RemoveFileFromBuild(targetGuid, fileGuid);
+                pbxProject.RemoveFile(fileGuid);
+            }
+            
+            // 移除文件夹引用
+            var subDirRealPaths = Directory.GetDirectories(realPath);
+            foreach (var subDirRealPath in subDirRealPaths)
+            {
+                var dirName = Path.GetDirectoryName(subDirRealPath);
+                var subDirProjectPath = $"{rootProjectPath}/{dirName}";
+                
+                if (subDirRealPath.EndsWith(FileExtensions.XcodeAssetsFile))
+                {
+                    var fileGuid = pbxProject.FindFileGuidByProjectPath(subDirProjectPath);
+                    if (string.IsNullOrEmpty(fileGuid)) 
+                        continue;
+                    
+                    pbxProject.RemoveFileFromBuild(targetGuid, fileGuid);
+                    pbxProject.RemoveFile(fileGuid);
+                }
+                else if (subDirRealPath.EndsWith(FileExtensions.XcodeUniversalFrameworkFile) || subDirRealPath.EndsWith(FileExtensions.XcodeFrameworkFile))
+                {
+                    var fileGuid = pbxProject.FindFileGuidByProjectPath(subDirProjectPath);
+                    if (!string.IsNullOrEmpty(fileGuid)) 
+                        pbxProject.RemoveFrameworkFromProject(targetGuid, fileGuid);
+                }
+                else
+                {
+                    pbxProject.RemoveGroupByProjectPath(targetGuid, subDirRealPath, subDirProjectPath);
                 }
             }
         }
