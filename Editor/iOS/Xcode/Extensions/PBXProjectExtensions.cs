bb1a824c8455b37597b3bcdb60a8f32614490098
Jerry Lee <cosmos53076@163.com>
2025-08-08 19:53:43 +0800

    Fixed bug of PBXProjectExtensions.CreateGroup method.

--- Editor/iOS/Xcode/Extensions/PBXProjectExtensions.cs
+++ Editor/iOS/Xcode/Extensions/PBXProjectExtensions.cs
@@ -19,13 +19,17 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
         /// Adds a new directory reference to the list of known files as "Create groups" for UnityMain target.
         /// </summary>
         /// <param name="pbxProject">The Xcode project. </param>
+        /// <param name="projectDirPath">The Xcode project directory path. </param>
         /// <param name="realPath">The physical path to the folder on the filesystem. </param>
         /// <param name="rootProjectPath">The root project path of the group to be created. </param>
+        /// <param name="fileExtensionsExclusive">The exclusive files list by extension. </param>
         /// <param name="filesExclusive">The exclusive files list. </param>
         /// <param name="dirsExclusive">The exclusive directories list. </param>
         public static void CreateGroup(this PBXProject pbxProject,
+            string projectDirPath,
             string realPath,
             string rootProjectPath,
+            string[] fileExtensionsExclusive = null,
             string[] filesExclusive = null,
             string[] dirsExclusive = null)
         {
@@ -37,18 +41,25 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
         /// Adds a new directory reference to the list of known files as "Create groups" for the specified target.
         /// </summary>
         /// <param name="pbxProject">The Xcode project. </param>
+        /// <param name="projectDirPath">The Xcode project directory path. </param>
         /// <param name="targetGuid">The GUID of the target as returned by <see cref="PBXProject.TargetGuidByName"/>. </param>
         /// <param name="realPath">The physical path to the folder on the filesystem. </param>
         /// <param name="rootProjectPath">The root project path of the group to be created. </param>
+        /// <param name="fileExtensionsExclusive">The exclusive files list by extension. </param>
         /// <param name="filesExclusive">The exclusive files list. </param>
         /// <param name="dirsExclusive">The exclusive directories list. </param>
         public static void CreateGroup(this PBXProject pbxProject,
+            string projectDirPath,
             string targetGuid,
             string realPath,
             string rootProjectPath,
+            string[] fileExtensionsExclusive = null,
             string[] filesExclusive = null,
             string[] dirsExclusive = null)
         {
+            var projectDirPathChars = projectDirPath.ToCharArray();
+            var dirSeparatorChar = Path.DirectorySeparatorChar;
+            
             // Adds file references. 
             var files = Directory.GetFiles(realPath);
             foreach (var file in files)
@@ -57,11 +68,32 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
                     continue;
 
                 var fileName = Path.GetFileName(file);
+                var fileExtension = Path.GetExtension(file);
                 if (filesExclusive != null && filesExclusive.Contains(fileName))
                     continue;
 
+                if (!string.IsNullOrEmpty(fileExtension) && fileExtensionsExclusive != null && fileExtensionsExclusive.Contains(fileExtension))
+                    continue;
+
                 var fileProjectPath = Path.Combine(rootProjectPath, fileName);
-                var fileGuid = pbxProject.AddFile(fileProjectPath, fileProjectPath);
+                var fileGuid = pbxProject.AddFile(file, fileProjectPath);
+
+                switch (fileExtension)
+                {
+                    case ".h":
+                        pbxProject.AddHeadersBuildPhase(targetGuid);
+                        break;
+                    
+                    case ".a":
+                    {
+                        const string librarySearchPathsPropertyName = "LIBRARY_SEARCH_PATHS";
+                        var path = file.TrimStart(projectDirPathChars).TrimStart(dirSeparatorChar).TrimEnd(fileName.ToCharArray()).TrimEnd(dirSeparatorChar);
+                        var propertyValue = $"$(PROJECT_DIR)/{path}";
+                        pbxProject.AddBuildProperty(targetGuid, librarySearchPathsPropertyName, propertyValue);
+                        break;
+                    }
+                }
+
                 pbxProject.AddFileToBuild(targetGuid, fileGuid);
             }
 
@@ -69,24 +101,39 @@ namespace UniSharperEditor.iOS.Xcode.Extensions
             var subDirRealPaths = Directory.GetDirectories(realPath);
             foreach (var subDirRealPath in subDirRealPaths)
             {
-                var dirName = Path.GetFileName(subDirRealPath);
-                if (dirsExclusive != null && dirsExclusive.Contains(dirName))
+                var subDirName = Path.GetFileName(subDirRealPath);
+                if (dirsExclusive != null && dirsExclusive.Contains(subDirName))
                     continue;
 
-                var subDirProjectPath = Path.Combine(rootProjectPath, dirName);
+                var subDirProjectPath = Path.Combine(rootProjectPath, subDirName);
                 if (subDirRealPath.EndsWith(FileExtensions.XcodeAssetsFile))
                 {
-                    var fileGuid = pbxProject.AddFile(subDirProjectPath, subDirProjectPath);
+                    var subDirExtension = Path.GetExtension(subDirRealPath);
+
+                    if (filesExclusive != null && filesExclusive.Contains(subDirName))
+                        continue;
+
+                    if (!string.IsNullOrEmpty(subDirExtension) && fileExtensionsExclusive != null && fileExtensionsExclusive.Contains(subDirExtension))
+                        continue;
+
+                    var fileGuid = pbxProject.AddFile(subDirRealPath, subDirProjectPath);
                     pbxProject.AddFileToBuild(targetGuid, fileGuid);
                 }
                 else if (subDirRealPath.EndsWith(FileExtensions.XcodeUniversalFrameworkFile) || subDirRealPath.EndsWith(FileExtensions.XcodeFrameworkFile))
                 {
                     var fileGuid = pbxProject.AddFile(subDirRealPath, subDirProjectPath);
+                    pbxProject.AddFileToBuild(targetGuid, fileGuid);
                     pbxProject.AddFileToEmbedFrameworks(targetGuid, fileGuid);
+                    
+                    // Add path to Framework Search Paths build settings
+                    const string frameworkSearchPathsPropertyName = "FRAMEWORK_SEARCH_PATHS";
+                    var path = subDirRealPath.TrimStart(projectDirPathChars).TrimStart(dirSeparatorChar).TrimEnd(subDirName.ToCharArray()).TrimEnd(dirSeparatorChar);
+                    var propertyValue = $"$(PROJECT_DIR)/{path}";
+                    pbxProject.AddBuildProperty(targetGuid, frameworkSearchPathsPropertyName, propertyValue);
                 }
                 else
                 {
-                    pbxProject.CreateGroup(targetGuid, subDirRealPath, subDirProjectPath, filesExclusive, dirsExclusive);
+                    pbxProject.CreateGroup(projectDirPath, targetGuid, subDirRealPath, subDirProjectPath, fileExtensionsExclusive, filesExclusive, dirsExclusive);
                 }
             }
         }
